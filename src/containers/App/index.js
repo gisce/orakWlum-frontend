30f5edb87433bfbcb8b9674c3913e051a3b92366
Xavi Torelló <info@xaviertorello.cat>
2016-10-17 13:08:14 +0200

    Global theme definition

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+/* theme creation */
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import {
+    deepOrange500,
+    deepOrange300,
+    yellow500,
+    blue500,
+    white,
+
+    orange700,
+    orange400,
+    amber400,
+    amber200,
+
+} from 'material-ui/styles/colors';
+
+const muiTheme = getMuiTheme({
+    palette: {
+        primary1Color: orange400,
+        primary2Color: orange400,
+        primary3Color: amber200,
+    },
+});
 
 /* application components */
 import { Header } from '../../components/Header';
@@ -10,6 +33,7 @@ import { Footer } from '../../components/Footer';
 /* global styles for app */
 import './styles/app.scss';
 
+
 class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
     static propTypes = {
         children: React.PropTypes.node,
@@ -17,7 +41,7 @@ class App extends React.Component { // eslint-disable-line react/prefer-stateles
 
     render() {
         return (
-            <MuiThemeProvider muiTheme={getMuiTheme()}>
+            <MuiThemeProvider muiTheme={muiTheme}>
                 <section>
                     <Header />
                     <div
