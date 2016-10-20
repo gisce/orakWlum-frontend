b9473f15766a49a6145b8d0e7224624991e4ef91
Xavi Torelló <info@xaviertorello.cat>
2016-10-20 17:30:31 +0200

    Breadcrumb solved BUG with paths and silent hrefs

--- src/components/Breadcrumb/index.js
+++ src/components/Breadcrumb/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actionCreators from '../../actions/data';
+import { browserHistory } from 'react-router';
 
 function mapStateToProps(state) {
     return {
@@ -15,31 +16,49 @@ function mapDispatchToProps(dispatch) {
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class Breadcrumb extends React.Component {
+    dispatchNewRoute(route) {
+        browserHistory.push(route);
+        this.setState({
+            open: false,
+        });
+
+    }
 
     render() {
 
-        let sectionUrl="";
+        let sectionUrlold="";
+        let sectionUrl=[];
         let classActive="";
 
         const path_list = this.props.path.split("/");
         const breadcrumbLen = path_list.length;
+
+        path_list[0]="";
 
         return (
             <div>
                 <ul className="breadcrumb">
                     {
                         path_list.map((section, index) => {
-                            if ( index === 0 ) return null;
+                            if ( index != 0 ) {
 
-                            classActive="";
-                            sectionUrl += "/" + section;
+                                classActive="";
 
-                            if (breadcrumbLen === index + 1) {
-                                classActive="active";
-                                sectionUrl="#";
+                                sectionUrlold += "/" + section;
+                                sectionUrl[index] = sectionUrlold;
+
+                                if (breadcrumbLen === index + 1) {
+                                    classActive="active";
+                                }
+                                section = section[0].toUpperCase() + section.slice(1);
+                                return <li
+                                            onClick={() => this.dispatchNewRoute(sectionUrl[index])}
+                                            key={index}
+                                            className={classActive}
+                                        >
+                                            <a className={classActive}>{section}</a>
+                                        </li>
                             }
-                            section = section[0].toUpperCase() + section.slice(1);
-                            return <li key={index} className={classActive}><a className={classActive} href={sectionUrl}>{section}</a></li>
                         })
                     }
                 </ul>
