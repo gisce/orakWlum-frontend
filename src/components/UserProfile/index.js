62459d0219bfeae5d3b0c0a5499046afcb3d294a
Xavi Torelló <info@xaviertorello.cat>
2016-10-24 13:25:00 +0200

    UserProfile component definition
    UserProfile lists
    * email
    * name
    * surname
    
    It handle if the view must show or edit the data
    
    Data setters with API WIP

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -1,103 +1,232 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as actionCreators from '../../actions/data';
-import { browserHistory } from 'react-router';
 
 
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
+import TextField from 'material-ui/TextField';
+
+import Avatar from 'material-ui/Avatar';
+import Chip from 'material-ui/Chip';
+import {orange300, orange900, green300, green900, red300, red900} from 'material-ui/styles/colors';
+
+
+import * as actionCreators from '../../actions/proposal';
+
+import { ProposalTag } from '../ProposalTag';
+
+function handleRequestDelete() {
+    alert('Treure TAG.');
+}
+
+function handleTouchTap() {
+    alert('Filtrar per aquest TAG.');
+}
+
+const styles = {
+    chip: {
+      margin: 4,
+    },
+    wrapper: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+};
+
+const colors = {
+    pending: {
+        hard: orange900,
+        soft: orange300,
+        text: 'white',
+    },
+    accepted: {
+        hard: green900,
+        soft: green300,
+        text: 'white',
+    },
+    denied: {
+        hard: red900,
+        soft: red300,
+        text: 'white',
+    },
+}
 
 function mapStateToProps(state) {
     return {
+        token: state.auth.token,
+        userName: state.auth.userName,
+        isAuthenticated: state.auth.isAuthenticated,
+        profile: state.profile,
     };
 }
-
 
 function mapDispatchToProps(dispatch) {
     return bindActionCreators(actionCreators, dispatch);
 }
 
 @connect(mapStateToProps, mapDispatchToProps)
-export default class Breadcrumb extends React.Component {
+export class UserProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            profile: props.profile,
+            editing: false,
+        };
+    }
+
     dispatchNewRoute(route) {
         browserHistory.push(route);
+    }
+
+    edit_profile() {
         this.setState({
-            open: false,
+            editing: true,
         });
 
     }
 
+    save_profile() {
+        this.setState({
+            editing: false,
+        });
+    }
+
+    discard_edit_profile() {
+        this.setState({
+            editing: false,
+        });
+    }
+
+    delete_profile() {
+        this.setState({
+            editing: false,
+        });
+    }
+
     render() {
+        let editing = this.state.editing;;
 
-        let sectionUrlold="";
-        let sectionUrl=[];
-        let classActive="";
+        const profile = this.props.profile.data;
+        const UserProfile = () => (
+            <Card>
+                <CardHeader
+                  title={profile.email}
+                  subtitle={profile.groups}
+                  avatar={profile.image}
+                />
+              <CardTitle
+                  title={profile.email}
+                  subtitle={profile.groups}
+              />
 
-        const path_list = this.props.path.split("/");
-        const breadcrumbLen = path_list.length;
+          {
+          ( !editing ) ?
+                <div>
+                  <CardText>
+                      <form role="form">
+                          <div className="row">
+                              <div className="col-md-4">
+                                  <TextField
+                                    hintText="Your name..."
+                                    floatingLabelText="Name"
+                                    value={profile.name}
+                                  />
+                              </div>
 
-        path_list[0]="";
+                              <div className="col-md-4">
+                                  <TextField
+                                    hintText="Your surname..."
+                                    floatingLabelText="Surname"
+                                    value={profile.surname}
+                                  />
+                              </div>
+                          </div>
+
+                          <div className="row">
+                              <div className="col-md-12">
+                                  <TextField
+                                    hintText="user@domain.com"
+                                    floatingLabelText="Email"
+                                    value={profile.email}
+                                  />
+                              </div>
+                          </div>
+                      </form>
+                  </CardText>
+
+                  <CardActions>
+                    <FlatButton
+                        onClick={(e) => this.edit_profile(e)}
+                        label="Edit" />
+                    <FlatButton
+                        onClick={(e) => this.delete_profile(e)}
+                        label="Delete" />
+                  </CardActions>
+              </div>
+
+              :
+              <div>
+                  <CardText>
+                      <form role="form">
+                          <div className="row">
+                              <div className="col-md-4">
+                                  <TextField
+                                    hintText="Your name..."
+                                    floatingLabelText="Name"
+                                    defaultValue={profile.name}
+                                    floatingLabelFixed={true}
+                                  />
+                              </div>
+
+                              <div className="col-md-4">
+                                  <TextField
+                                    hintText="Your surname..."
+                                    floatingLabelText="Surname"
+                                    defaultValue={profile.surname}
+                                    floatingLabelFixed={true}
+                                  />
+                              </div>
+                          </div>
+
+                          <div className="row">
+                              <div className="col-md-12">
+                                  <TextField
+                                    hintText="user@domain.com"
+                                    floatingLabelText="Email"
+                                    defaultValue={profile.email}
+                                    floatingLabelFixed={true}
+                                  />
+                              </div>
+                          </div>
+                      </form>
+                  </CardText>
+                  <CardActions>
+                    <FlatButton
+                        onClick={(e) => this.save_profile(e)}
+                        label="Save" />
+                    <FlatButton
+                        onClick={(e) => this.discard_edit_profile(e)}
+                        label="Cancel" />
+                    <FlatButton
+                        onClick={(e) => this.delete_profile(e)}
+                        label="Delete" />
+                  </CardActions>
+              </div>
+          }
+
+            </Card>
+        );
 
         return (
-            <div>
-
-              <Card>
-                <CardHeader
-                  title="URL Avatar"
-                  subtitle="Subtitle"
-                  avatar="images/jsa-128.jpg"
-                />
-                <CardMedia
-                  overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-                >
-                  <img src="images/nature-600-337.jpg" />
-                </CardMedia>
-                <CardTitle title="Card title" subtitle="Card subtitle" />
-                <CardText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                  Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-                  Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-                </CardText>
-                <CardActions>
-                  <FlatButton label="Action1" />
-                  <FlatButton label="Action2" />
-                </CardActions>
-              </Card>
-
-                <ul className="breadcrumb">
-                    {
-                        path_list.map((section, index) => {
-                            if ( index != 0 ) {
-
-                                classActive="";
-
-                                sectionUrlold += "/" + section;
-                                sectionUrl[index] = sectionUrlold;
-
-                                if (breadcrumbLen === index + 1) {
-                                    classActive="active";
-                                }
-
-                                section = section[0].toUpperCase() + section.slice(1);
-                                return <li
-                                            onClick={() => this.dispatchNewRoute(sectionUrl[index])}
-                                            key={index}
-                                            className={classActive}
-                                        >
-                                            <a className={classActive}>{section}</a>
-                                        </li>
-                            }
-                        })
-                    }
-                </ul>
-            </div>
+            <UserProfile />
         );
     }
 }
 
-Breadcrumb.propTypes = {
-    path: React.PropTypes.string,
+UserProfile.propTypes = {
+    logoutAndRedirect: React.PropTypes.func,
+    isAuthenticated: React.PropTypes.bool,
 };
