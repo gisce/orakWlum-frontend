739c413b2361ae3683cfa4e72fef5727a3a467ae
Xavi Torelló <info@xaviertorello.cat>
2016-10-27 13:50:26 +0200

    Profile Actions for Update Profile

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -1,4 +1,4 @@
-import { FETCH_PROFILE_REQUEST, RECEIVE_PROFILE } from '../constants/index'
+import { FETCH_PROFILE_REQUEST, RECEIVE_PROFILE, UPDATE_PROFILE_REQUEST, RECEIVE_UPDATE_PROFILE } from '../constants/index'
 import { data_fetch_api_resource } from '../utils/http_functions'
 import { parseJSON } from '../utils/misc'
 import { logoutAndRedirect } from './auth'
@@ -18,9 +18,42 @@ export function fetchProfileRequest() {
     };
 }
 
+
+export function receiveUpdateProfile(data) {
+    return {
+        type: RECEIVE_UPDATE_PROFILE,
+        payload: {
+            data,
+        },
+    };
+}
+
+
+export function updateProfileRequest() {
+    return {
+        type: UPDATE_PROFILE_REQUEST,
+    };
+}
 export function fetchProfile(token) {
     return (dispatch) => {
         dispatch(fetchProfileRequest());
+        data_fetch_api_resource(token, "user/" )
+            .then(parseJSON)
+            .then(response => {
+                dispatch(receiveProfile(response.result));
+            })
+            .catch(error => {
+                if (error.status === 401) {
+                    dispatch(logoutAndRedirect(error));
+                }
+            });
+    };
+}
+
+
+export function updateProfile(token) {
+    return (dispatch) => {
+        dispatch(updateProfileRequest());
         data_fetch_api_resource(token, "user/" )
             .then(parseJSON)
             .then(response => {
