28e1f9777c12374cb6f8a0f73ea7f44213d5e5de
Xavi Torelló <info@xaviertorello.cat>
2016-10-27 11:56:36 +0200

    LoginView can use next GET parameter to change next route

--- src/components/LoginView.js
+++ src/components/LoginView.js
@@ -110,7 +110,18 @@ export default class LoginView extends React.Component {
 
     login(e) {
         e.preventDefault();
-        this.props.loginUser(this.state.email, this.state.password, this.state.redirectTo);
+
+
+        const next = "/" +  (this.props.location.query.next)?this.props.location.query.next:null;
+
+        this.setState({
+            redirectTo: next
+        })
+
+        console.log("redirect to", this.state.redirectTo, next);
+
+
+        this.props.loginUser(this.state.email, this.state.password, next);
     }
 
     help(e) {
