e380f780e59f1ddcb83f3a2a0666c0715f75ef9f
Xavi Torelló <info@xaviertorello.cat>
2016-11-08 15:22:56 +0100

    Proposal development Redux Action and Reducer

---
+++ src/reducers/proposal0.js
@@ -0,0 +1,21 @@
+import { RECEIVE_PROPOSAL, FETCH_PROPOSAL_REQUEST } from '../constants';
+import { createReducer } from '../utils/misc';
+
+const initialState = {
+    data: null,
+    isFetching: false,
+    loaded: false,
+};
+
+export default createReducer(initialState, {
+    [RECEIVE_PROPOSAL0]: (state, payload) =>
+        Object.assign({}, state, {
+            data: payload.data,
+            isFetching: false,
+            loaded: true,
+        }),
+    [FETCH_PROPOSAL0_REQUEST]: (state) =>
+        Object.assign({}, state, {
+            isFetching: true,
+        }),
+});
