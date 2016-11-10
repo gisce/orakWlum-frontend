91d7dc54ab9a9601ae6a938ab56703c7bea4cb58
Xavi Torelló <info@xaviertorello.cat>
2016-11-10 13:40:16 +0100

    Debug handle debug definition at storage

--- src/utils/debug.js
+++ src/utils/debug.js
@@ -2,8 +2,11 @@
 import React from 'react';
 
 export function debug(what) {
+    var debug = false;
+    debug = localStorage.getItem('debug');
 
     return (
+        debug &&
         <div>
             <h3>Debug:</h3>
             <pre>{ JSON.stringify(what, null, 2) }</pre>
