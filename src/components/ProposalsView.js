cadea269582e71982d7690d831d734afce8cc765
Xavi Torelló <info@xaviertorello.cat>
2016-11-10 12:41:56 +0100

    Integrate DEBUG into Proposals View

--- src/components/ProposalsView.js
+++ src/components/ProposalsView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actionCreators from '../actions/proposals';
+import { debug } from '../utils/debug';
 
 import { ProposalList } from './ProposalList';
 
@@ -42,12 +43,9 @@ export default class ProposalsView extends React.Component {
                             proposals={this.props.data.data}
                             path={this.props.location.pathname}
                         />
-
-                        <h3>Proposals:</h3>
-                        <pre>{ JSON.stringify(this.props.data, null, 2) }</pre>
-
                     </div>
                 }
+                {debug(this.props.data)}
             </div>
         );
     }
@@ -57,7 +55,6 @@ ProposalsView.propTypes = {
     fetchProtectedDataProposals: React.PropTypes.func,
     fetchProtectedData: React.PropTypes.func,
     loaded: React.PropTypes.bool,
-    userName: React.PropTypes.string,
     data: React.PropTypes.any,
     token: React.PropTypes.string,
 };
