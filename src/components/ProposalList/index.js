dcbdd1132cab6373237690cbfded79c7aa49847b
Xavi Torelló <info@xaviertorello.cat>
2016-11-10 13:27:21 +0100

    ProposalList BUG with IDs, now using the correct ID

--- src/components/ProposalList/index.js
+++ src/components/ProposalList/index.js
@@ -92,7 +92,7 @@ export class ProposalList extends Component {
                       titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
                       cols={index < howManyBig ? 2 : 1}
                       rows={index < howManyBig ? 2 : 1}
-                      onClick={() => this.dispatchNewRoute(this.state.path + (index +1))}
+                      onClick={() => this.dispatchNewRoute(this.state.path + (tile.id))}
                       style={styles.gridTile}
                     >
                     <div><br/><br/><br/><br/></div>
