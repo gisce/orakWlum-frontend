a32d7e53c178ecbb5228ad379c177b3c17711a66
Xavi Torelló <info@xaviertorello.cat>
2016-11-10 13:25:53 +0100

    Proposal Actions with new constants

--- src/actions/proposal.js
+++ src/actions/proposal.js
@@ -1,11 +1,11 @@
-import { FETCH_PROPOSAL0_REQUEST, RECEIVE_PROPOSAL0 } from '../constants/index'
+import { FETCH_PROPOSAL_REQUEST, RECEIVE_PROPOSAL } from '../constants/index'
 import { data_fetch_api_resource } from '../utils/http_functions'
 import { parseJSON } from '../utils/misc'
 import { logoutAndRedirect } from './auth'
 
 export function receiveProposal(data) {
     return {
-        type: RECEIVE_PROPOSAL0,
+        type: RECEIVE_PROPOSAL,
         payload: {
             data,
         },
@@ -14,7 +14,7 @@ export function receiveProposal(data) {
 
 export function fetchProposalRequest() {
     return {
-        type: FETCH_PROPOSAL0_REQUEST,
+        type: FETCH_PROPOSAL_REQUEST,
     };
 }
 
