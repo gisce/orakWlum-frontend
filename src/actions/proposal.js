2701aeaea34f9e5a48b448eb41ed4c32660cf5fb
Xavi Torelló <info@xaviertorello.cat>
2016-11-28 11:50:37 +0100

    Proposal reRunProposal action
    Consume /api/v1/proposal/$ID/run
    * Force API to recalc the proposal
    * Return the expected result
    * The result is passed as the current proposal, and a re-render of all
     the involved elements is called

--- src/actions/proposal.js
+++ src/actions/proposal.js
@@ -1,4 +1,4 @@
-import { FETCH_PROPOSAL_REQUEST, RECEIVE_PROPOSAL, FETCH_AGGREGATIONS_REQUEST, RECEIVE_AGGREGATIONS } from '../constants/index'
+import { FETCH_PROPOSAL_REQUEST, RUN_PROPOSAL_REQUEST, RECEIVE_PROPOSAL, FETCH_AGGREGATIONS_REQUEST, RECEIVE_AGGREGATIONS } from '../constants/index'
 import { data_fetch_api_resource } from '../utils/http_functions'
 import { parseJSON } from '../utils/misc'
 import { logoutAndRedirect } from './auth'
@@ -19,6 +19,29 @@ export function fetchProposalRequest() {
     };
 }
 
+export function runProposalRequest() {
+    return {
+        type: RUN_PROPOSAL_REQUEST,
+    };
+}
+
+export function runProposal(token, proposal) {
+    return (dispatch) => {
+        dispatch(runProposalRequest());
+        data_fetch_api_resource(token, "proposal/" + proposal + "/run/")
+            .then(parseJSON)
+            .then(response => {
+                dispatch(receiveProposal(response.result, response.aggregations));
+            })
+            .catch(error => {
+                if (error.status === 401) {
+                    dispatch(logoutAndRedirect(error));
+                }
+            });
+    };
+}
+
+
 export function fetchProposal(token, proposal) {
     return (dispatch) => {
         dispatch(fetchProposalRequest());
@@ -34,6 +57,8 @@ export function fetchProposal(token, proposal) {
             });
     };
 }
+
+
 
 export function fetchAggregationsRequest() {
     return {
