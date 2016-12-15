dedb02f100cca4cb1271d9171c74d89ecc01e335
Xavi Torelló <info@xaviertorello.cat>
2016-12-15 16:23:52 +0100

    Add Floating RefreshButton to <ContentHeader> -> Proposals View
    Extend ContentHeader button to provide a refresh button
    
    Fix #63

--- src/components/ContentHeader/index.js
+++ src/components/ContentHeader/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import Refresh from 'material-ui/svg-icons/navigation/refresh';
 
 const styles = {
     buttonAdd: {
@@ -24,6 +25,14 @@ export class ContentHeader extends Component {
         const withButton = (this.props.addButton)?this.props.addButton:false;
         const onClickMethod = (this.props.buttonClickMethod)?this.props.buttonClickMethod:null;
 
+        // The Refresh Button
+        const refreshButton = (
+            <FloatingActionButton style={styles.buttonAdd} onClick={() => onClickMethod()}>
+                  <Refresh />
+            </FloatingActionButton>
+        )
+
+        // The Add Button
         const addButton = (
             <FloatingActionButton style={styles.buttonAdd} onClick={() => onClickMethod()}>
                   <ContentAdd />
@@ -34,7 +43,10 @@ export class ContentHeader extends Component {
             <div className='row'>
                 <div className="col-md-6"><h1>{title}</h1></div>
                     { withButton &&
-                        <div className="col-md-6" style={styles.buttonPosition}>{addButton}</div>
+                        <div className="col-md-6" style={styles.buttonPosition}>
+                            {refreshButton}
+                            {addButton}
+                        </div>
                     }
             </div>
 
