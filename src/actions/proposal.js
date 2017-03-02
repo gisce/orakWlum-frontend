7fa9ea5a0d95b2f9fd930bb930b22c3151eac1aa
Xavi Torelló <info@xaviertorello.cat>
2017-03-02 15:31:31 +0100

    Add Export Proposal actions

--- src/actions/proposal.js
+++ src/actions/proposal.js
@@ -1,4 +1,4 @@
-import { FETCH_PROPOSAL_REQUEST, RUN_PROPOSAL_REQUEST, RECEIVE_PROPOSAL, RECEIVE_RUN_PROPOSAL, RECEIVE_RUN_PROPOSAL_ERROR, FETCH_AGGREGATIONS_REQUEST, RECEIVE_AGGREGATIONS, DUPLICATE_PROPOSAL_REQUEST, DELETE_PROPOSAL_REQUEST, CREATE_PROPOSAL_REQUEST } from '../constants/index'
+import { EXPORT_PROPOSAL_REQUEST, FETCH_PROPOSAL_REQUEST, RUN_PROPOSAL_REQUEST, RECEIVE_PROPOSAL, RECEIVE_RUN_PROPOSAL, RECEIVE_RUN_PROPOSAL_ERROR, FETCH_AGGREGATIONS_REQUEST, RECEIVE_AGGREGATIONS, DUPLICATE_PROPOSAL_REQUEST, DELETE_PROPOSAL_REQUEST, CREATE_PROPOSAL_REQUEST } from '../constants/index'
 import { data_fetch_api_resource, data_create_api_resource, data_delete_api_resource } from '../utils/http_functions'
 import { parseJSON } from '../utils/misc'
 import { logoutAndRedirect, redirectToRoute } from './auth'
@@ -293,6 +293,38 @@ export function fetchAggregations(token) {
             .then(parseJSON)
             .then(response => {
                 dispatch(receiveAggregations(response.result));
+            })
+            .catch(error => {
+                if (error.status === 401) {
+                    dispatch(logoutAndRedirect(error));
+                }
+            });
+    };
+}
+
+
+
+
+
+/*********************
+  #################
+   EXPORT PROPOSAL
+  #################
+*********************/
+
+export function exportProposalRequest() {
+    return {
+        type: EXPORT_PROPOSAL_REQUEST,
+    };
+}
+
+export function exportProposal(token, proposal) {
+    return (dispatch) => {
+        dispatch(exportProposalRequest());
+        data_fetch_api_resource(token, "proposal/" + proposal + "/xls/")
+            .then(response => {
+               FileSaver.saveAs(blob, "asdadasdasd");
+
             })
             .catch(error => {
                 if (error.status === 401) {
