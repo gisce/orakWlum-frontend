27bce66b6ba9a425d4f15fa284f6df194a7f5371
Xavi Torelló <info@xaviertorello.cat>
2017-04-26 15:18:45 +0200

    <SettingSources> now handle reload and lite flag
    * reload -> avoids the passed measures and static_data and trigger the
     Settings action to refetch sources
    * lite -> render just the measures SmartTable

--- src/components/SettingsSources/index.js
+++ src/components/SettingsSources/index.js
@@ -1,82 +1,127 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as actionCreators from '../../actions/settings';
 
 import { SmartTable } from '../SmartTable'
 
 const styles = {
 };
 
-export class SettingsSources extends Component {
+function mapStateToProps(state) {
+    return {
+        settings: state.settings,
+        token: state.auth.token,
+        loaded: state.settings.loaded,
+        isFetching: state.settings.isFetching,
+        error: state.settings.error,
+        errorMessage: state.settings.data,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actionCreators, dispatch);
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
+export class SettingsSources extends React.Component {
+    componentDidMount() {
+        this.props.reload &&
+            this.fetchData();
+    }
+
+    fetchData() {
+        const token = this.props.token;
+        this.props.fetchSettings(token);
+    }
+
+    updateData(data) {
+        const token = this.props.token;
+        this.props.updateSettings(token, data);
+    }
 
     render() {
-        const {measures, static_data} = this.props;
+        if (this.props.loaded) {
 
-        //Adapt measures
-        const measures_adapted = measures.map(function( entry, index){
-            const active = (entry.active)?"Active":"Deactivated";
-            const db_fields = entry.config[0] + ":" + entry.config[1] + "@" + entry.config[2] + "/" + entry.config[3];
-            return (
-                [
-                    entry.name,
-                    entry.alias,
-                    entry.type,
-                    entry.unit,
-                    db_fields,
-                    active,
-                ]
+            const {measures, static_data} = (this.props.reload)?
+                 this.props.settings.data
+                 :
+                 this.props;
+
+            //Adapt measures
+            const measures_adapted = measures.map(function( entry, index){
+                const active = (entry.active)?"Active":"Deactivated";
+                const db_fields = entry.config[0] + ":" + entry.config[1] + "@" + entry.config[2] + "/" + entry.config[3];
+                return (
+                    [
+                        entry.name,
+                        entry.alias,
+                        entry.type,
+                        entry.unit,
+                        db_fields,
+                        active,
+                    ]
+                )
+            })
+
+            //Adapt static data
+            const static_data_adapted = static_data.map(function( entry, index){
+                const active = (entry.active)?"Active":"Deactivated";
+                const db_fields = entry.config[0] + ":" + entry.config[1] + "@" + entry.config[2] + "/" + entry.config[3];
+                return (
+                    [
+                        entry.name,
+                        entry.alias,
+                        entry.type,
+                        entry.unit,
+                        db_fields,
+                        active,
+                    ]
+                )
+            })
+
+            const headers = [
+                {
+                    title: 'Name',
+                    width: null,
+                },                {
+                    title: 'Alias',
+                    width: null,
+                },                {
+                    title: 'Type',
+                    width: '10%',
+                },                {
+                    title: 'Unit',
+                    width: '10%',
+                },                {
+                    title: 'DB',
+                    width: '30%',
+                },                {
+                    title: 'Status',
+                    width: null,
+                },
+            ];
+
+            const Settings = (this.props.lite)?
+            <SmartTable header={headers} data={measures_adapted}/>
+            :
+            (
+                <div>
+                    <h2>Available sources</h2>
+                    <SmartTable title="Measures" header={headers} data={measures_adapted}/>
+                    <SmartTable title="Static Data" header={headers} data={static_data_adapted}/>
+                </div>
             )
-        })
-
-        //Adapt static data
-        const static_data_adapted = static_data.map(function( entry, index){
-            const active = (entry.active)?"Active":"Deactivated";
-            const db_fields = entry.config[0] + ":" + entry.config[1] + "@" + entry.config[2] + "/" + entry.config[3];
-            return (
-                [
-                    entry.name,
-                    entry.alias,
-                    entry.type,
-                    entry.unit,
-                    db_fields,
-                    active,
-                ]
-            )
-        })
-
-        const headers = [
-            {
-                title: 'Name',
-                width: null,
-            },                {
-                title: 'Alias',
-                width: null,
-            },                {
-                title: 'Type',
-                width: '10%',
-            },                {
-                title: 'Unit',
-                width: '10%',
-            },                {
-                title: 'DB',
-                width: '30%',
-            },                {
-                title: 'Status',
-                width: null,
-            },
-        ];
-
-        const Settings=(
-            <div>
-                <h2>Available sources</h2>
-                <SmartTable title="Measures" header={headers} data={measures_adapted}/>
-                <SmartTable title="Static Data" header={headers} data={static_data_adapted}/>
-            </div>
-        )
-
-        return Settings;
+            return Settings;
+        }
+        return null;
     }
 }
 
 SettingsSources.propTypes = {
-    measures: React.PropTypes.array.isRequired,
-    static_data: React.PropTypes.array.isRequired,
+    measures: React.PropTypes.array,
+    static_data: React.PropTypes.array,
+    reload: React.PropTypes.bool,
+    lite: React.PropTypes.bool,
 };
