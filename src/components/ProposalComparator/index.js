7941beb44bd9172b52272baeaa7b50650180a7eb
Xavi Torelló <info@xaviertorello.cat>
2017-05-09 13:23:35 +0200

    Prepare horizontal comparation

--- src/components/ProposalComparator/index.js
+++ src/components/ProposalComparator/index.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
 
 import { Proposal } from '../Proposal'
+import Paper from 'material-ui/Paper';
 
 const styles = {
+    horizontal: {
+        display: 'inline-block',
+        maxWidth: 530,
+        margin: 20,
+        textAlign: 'center',
+    }
+
 };
 
 export class ProposalComparator extends Component {
@@ -15,7 +23,47 @@ export class ProposalComparator extends Component {
     }
 
     render() {
+
         const {title, elementA, elementB} = this.props;
+
+        const ProposalA = (
+            <Proposal
+                proposal={elementA.element}
+                aggregations={elementA.aggregations}
+            />
+        );
+
+        const ProposalB = (
+            <Proposal
+                proposal={elementB.element}
+                aggregations={elementB.aggregations}
+            />
+        );
+
+        //Handle view mode
+
+        const mode = "horizontal";
+        const result = (mode == "vertical")?
+            (
+                <div>
+                    {ProposalA}
+                    <hr/>
+                    {ProposalB}
+                </div>
+            )
+            :
+            (
+                <div>
+                    <Paper style={styles.horizontal}>
+                        {ProposalA}
+                    </Paper>
+                    <Paper style={styles.horizontal}>
+                        {ProposalB}
+                    </Paper>
+                </div>
+            )
+        ;
+
 
         return  (
             <div>
@@ -25,10 +73,7 @@ export class ProposalComparator extends Component {
                     <h3>{title}</h3>
                 }
 
-                <Proposal
-                    proposal={elementA.element}
-                    aggregations={elementA.aggregations}
-                    />
+                {result}
 
             </div>
 
