67ec8042d92a03c24a52bfc6a888f62176bdf9ec
Xavi Torelló <info@xaviertorello.cat>
2017-05-09 12:56:21 +0200

    Add elements action

---
+++ src/actions/elements.js
@@ -0,0 +1,56 @@
+import {
+    FETCH_ELEMENTS_REQUEST,
+    RECEIVE_ELEMENTS
+} from '../constants/index'
+import {
+    data_fetch_api_resource
+} from '../utils/http_functions'
+import {
+    parseJSON
+} from '../utils/misc'
+import {
+    logoutAndRedirect
+} from './auth'
+
+
+export function fetchElementsRequest(initial) {
+    const message = (initial)?null:"Refreshing elements list";
+
+    return {
+        type: FETCH_ELEMENTS_REQUEST,
+        payload: {
+            message,
+        },
+    };
+}
+
+export function receiveElements(data, aggregations, initial) {
+    const message = (initial)?null:"Elements list updated";
+    return {
+        type: RECEIVE_ELEMENTS,
+        payload: {
+            data,
+            aggregations,
+            message,
+        },
+    };
+}
+
+export function fetchElements(token, elements, initial=false) {
+    return (dispatch) => {
+        dispatch(fetchElementsRequest(initial));
+        data_fetch_api_resource(token, "proposal/")
+            .then(parseJSON)
+            .then(response => {
+                console.log("receive!")
+                dispatch(receiveElements(response.result, response.aggregations, initial));
+            })
+            /*
+            .catch(error => {
+                if (error.status === 401) {
+                    dispatch(logoutAndRedirect(error));
+                }
+            });
+            */
+    };
+}
