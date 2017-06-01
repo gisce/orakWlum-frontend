c7f1bedc1be70e41fc56f8444cd6aef6615dfb42
Xavi Torelló <info@xaviertorello.cat>
2017-06-01 11:57:24 +0200

    Initial Elements dashboard component

---
+++ src/components/ElementsDashboard/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import {GridList, GridTile} from 'material-ui/GridList';
+import Subheader from 'material-ui/Subheader';
+import IconButton from 'material-ui/IconButton';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+
+import * as actionCreators from '../../actions/proposals';
+
+import { ProposalTag } from '../ProposalTag';
+import { ProposalGraph } from '../ProposalGraph';
+
+import { dispatchNewRoute} from '../../utils/http_functions';
+
+const styles = {
+};
+
+function mapStateToProps(state) {
+    return {
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actionCreators, dispatch);
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
+export class ElementsDashboard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        };
+    }
+
+    render() {
+
+    }
+}
+
+ElementsDashboard.propTypes = {
+};
