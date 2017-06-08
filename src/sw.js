15c9a09a3c753a623319eb0951262a11687be822
Xavi Torelló <info@xaviertorello.cat>
2017-06-08 16:22:54 +0200

    Update SW handler!

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,6 @@
+'use strict';
 
 // Register a listener for push events!
-self.addEventListener('push', () => {
-    console.log("new push!");
+self.addEventListener('push', (event) => {
+    console.log("new pushh!", event);
 });
