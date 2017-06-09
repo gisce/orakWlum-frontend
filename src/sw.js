d1a8206c3a71931bb683947dd65995d0bacc4997
Xavi Torelló <info@xaviertorello.cat>
2017-06-09 11:55:34 +0200

    Testing browser notifications

--- src/sw.js
+++ src/sw.js
@@ -3,4 +3,18 @@
 // Register a listener for push events!
 self.addEventListener('push', (event) => {
     console.log("new pushh!", event);
+
+    var title = 'A new message!';
+    var body = 'Testing browser notifications!';
+    var icon = '/images/the-icon-192x192.png';
+    var tag = 'notification-test';
+
+    // toDo ensure permissions are enought
+    event.waitUntil(
+      self.registration.showNotification(title, {
+        body: body,
+        icon: icon,
+        tag: tag
+      })
+    );
 });
