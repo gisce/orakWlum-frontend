e71c0c91ff22586ce992b68c1919102ea700774a
Xavi Torelló <info@xaviertorello.cat>
2017-06-13 14:44:54 +0200

    OVERRIDE_AGGREGATIONS reducer

--- src/reducers/orakwlum.js
+++ src/reducers/orakwlum.js
@@ -1,38 +1,40 @@
-import { RECEIVE_ELEMENTS, FETCH_ELEMENTS_REQUEST, OVERRIDE_ELEMENTS, OVERRIDE_MESSAGE } from '../constants';
-import { createReducer } from '../utils/misc';
+import {RECEIVE_ELEMENTS, FETCH_ELEMENTS_REQUEST, OVERRIDE_ELEMENTS, OVERRIDE_MESSAGE, OVERRIDE_AGGREGATIONS} from '../constants';
+import {createReducer} from '../utils/misc';
 
 const initialState = {
-    data: null,
     isFetching: false,
     loaded: false,
     message: "",
+    elements: {},
+    aggregations: {}
 };
 
 export default createReducer(initialState, {
-    [RECEIVE_ELEMENTS]: (state, payload) =>
-        Object.assign({}, state, {
-            elements: {...state.elements, ...payload.elements},
-            message: payload.message,
-            isFetching: false,
-            loaded: true,
-        }),
-    [OVERRIDE_ELEMENTS]: (state, payload) =>
-        Object.assign({}, state, {
-            elements: payload.elements,
-            message: payload.message,
-            isFetching: false,
-            loaded: true,
-        }),
-    [FETCH_ELEMENTS_REQUEST]: (state, payload) =>
-        Object.assign({}, state, {
-            isFetching: true,
-            message: payload.message,
-        }),
+    [RECEIVE_ELEMENTS]: (state, payload) => Object.assign({}, state, {
+        elements: {
+            ...state.elements,
+            ...payload.elements
+        },
+        message: payload.message,
+        isFetching: false,
+        loaded: true
+    }),
+    [OVERRIDE_ELEMENTS]: (state, payload) => Object.assign({}, state, {
+        elements: payload.elements,
+        message: payload.message,
+        isFetching: false,
+        loaded: true
+    }),
+    [OVERRIDE_AGGREGATIONS]: (state, payload) => Object.assign({}, state, {
+        aggregations: payload.aggregations,
+        message: payload.message,
+        isFetching: false,
+        loaded: true
+    }),
+    [FETCH_ELEMENTS_REQUEST]: (state, payload) => Object.assign({}, state, {
+        isFetching: true,
+        message: payload.message
+    }),
 
-
-    [OVERRIDE_MESSAGE]: (state, payload) =>
-        Object.assign({}, state, {
-            message: payload.message,
-        }),
-
+    [OVERRIDE_MESSAGE]: (state, payload) => Object.assign({}, state, {message: payload.message})
 });
