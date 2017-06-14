92bf5f57f0e3ba9113b20d995bb8b41a3117964c
Xavi Torelló <info@xaviertorello.cat>
2017-06-14 16:52:02 +0000

    Add Proptypes validation

--- src/components/Connection/index.js
+++ src/components/Connection/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import PropTypes from 'prop-types';
+
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actionCreators from '../../actions/orakwlum';
