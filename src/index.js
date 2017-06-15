6ae2190e3f98499b76b95416717205d017bbbad4
Xavi Torelló <info@xaviertorello.cat>
2017-06-15 11:07:15 +0000

    AppProvider class with loading once the rehydration is performed
    Fix #215

--- src/index.js
+++ src/index.js
@@ -12,6 +12,9 @@ import configureStore from './store/configureStore';
 import routes from './routes';
 import './style.scss';
 
+//Prepare Redux rehydration
+import {persistStore} from 'redux-persist';
+
 require('expose?$!expose?jQuery!jquery');
 require('bootstrap-webpack');
 
@@ -21,16 +24,47 @@ OfflinePluginRuntime.install({
     onUpdated: () => window.softwareUpdate = true,
 });
 
+//Tap event plugin
 injectTapEventPlugin();
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
 
+//AppProvider class with rehydration handling
+class AppProvider extends React.Component {
+  constructor() {
+    super()
+    this.state = { rehydrated: false }
+  }
+
+  componentWillMount(){
+    persistStore(store, {}, () => {
+      this.setState({ rehydrated: true })
+    })
+  }
+
+  render() {
+	//Show loadingAnimation until store is rehydrated
+    if(!this.state.rehydrated){
+		return (
+			<div>Loading okW...</div>
+		)
+    }
+
+    return (
+		<Provider store={store}>
+			<Router history={history}>
+				<Redirect from="/" to="elements" />
+				{routes}
+			</Router>
+		</Provider>
+    )
+  }
+}
+
+const the_app = <AppProvider />;
+
+//Render the app!
 ReactDOM.render(
-    <Provider store={store}>
-        <Router history={history}>
-            <Redirect from="/" to="elements" />
-            {routes}
-        </Router>
-    </Provider>,
+	the_app,
     document.getElementById('root')
 );
