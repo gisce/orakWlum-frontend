8e4b443ac49210653d37d0fac3bb3f99e3d8b946
Xavi Torelló <info@xaviertorello.cat>
2017-06-19 12:51:44 +0200

    Pass title also for injected onClick <ProposalList> method

--- src/components/ProposalList/index.js
+++ src/components/ProposalList/index.js
@@ -185,7 +185,7 @@ export class ProposalList extends Component {
                         titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
                         cols={index < howManyBig ? 2 : 1}
                         rows={index < howManyBig ? 2 : 1}
-                        onClick={(event) => (onclick)? onclick(tile.id) : dispatchNewRoute(this.state.path + (tile.id), event)}
+                        onClick={(event) => (onclick)? onclick(tile.id, title) : dispatchNewRoute(this.state.path + (tile.id), event)}
                         style={styles.gridTile}
                     >
                         <div><br/><br/><br/><br/></div>
