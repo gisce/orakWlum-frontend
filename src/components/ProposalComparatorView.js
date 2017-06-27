70b61f4406bf5e8caac049067ea38d69510854db
Xavi Torelló <info@xaviertorello.cat>
2017-06-27 12:54:27 +0200

    ProposalComparator integration with websocket actions

--- src/components/ProposalComparatorView.js
+++ src/components/ProposalComparatorView.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import * as actionCreators from '../actions/elements';
+import * as actionCreators from '../actions/orakwlum';
 import { debug } from '../utils/debug';
 
 import { ProposalComparator } from './ProposalComparator';
@@ -13,12 +13,9 @@ import { LoadingAnimation } from 'materialized-reactions/LoadingAnimation';
 function mapStateToProps(state) {
 
     return {
-        data: state.elements,
-        allAggregations: state.elements.allAggregations,
-        comparison: state.elements.comparison,
-        token: state.auth.token,
-        loaded: state.elements.loaded,
-        isFetching: state.elements.isFetching,
+        elements: state.orakwlum.elements,
+        elements_volatile: state.orakwlum.elements_volatile,
+        aggregations: state.orakwlum.aggregations,
     };
 }
 
@@ -31,22 +28,22 @@ export default class ProposalComparatorView extends React.Component {
     componentWillMount() {
         this.idA = this.props.params.elementA;
         this.idB = this.props.params.elementB;
+        this.idComp = "" + this.idA + "," + this.idB;
 
         this.fetchData();
     }
 
-    fetchData() {
-        const token = this.props.token;
-        this.props.fetchElementsByIDS(token, [this.idA, this.idB], true);
+    fetchData(initial=true) {
+        this.props.fetchComparation([this.idA, this.idB], initial);
     }
 
     render() {
-        const elements = this.props.data.data;
-        const {allAggregations, comparison} = this.props;
+        const {elements, aggregations, elements_volatile} = this.props;
 
-        if (elements != null && elements.length==2 && allAggregations != null) {
-            const elementA = elements[0];
-            const elementB = elements[1];
+        if (elements != null && this.idA in elements && this.idB in elements && elements_volatile && this.idComp in elements_volatile) {
+            const elementA = elements[this.idA];
+            const elementB = elements[this.idB];
+            const comparison = elements_volatile[this.idComp];
 
             //Prepare the Aggregations for each element
             let aggregationsListA = [];
@@ -54,16 +51,16 @@ export default class ProposalComparatorView extends React.Component {
             let aggregationsListComparison = [];
 
             elementA.aggregations.map( function(agg, i){
-                if (agg in allAggregations)
-                    aggregationsListA.push( allAggregations[agg]);
+                if (agg in aggregations)
+                    aggregationsListA.push( aggregations[agg]);
             })
             elementB.aggregations.map( function(agg, i){
-                if (agg in allAggregations)
-                    aggregationsListB.push( allAggregations[agg]);
+                if (agg in aggregations)
+                    aggregationsListB.push( aggregations[agg]);
             })
             comparison.aggregations.map( function(agg, i){
-                if (agg in allAggregations)
-                    aggregationsListComparison.push( allAggregations[agg]);
+                if (agg in aggregations)
+                    aggregationsListComparison.push( aggregations[agg]);
             })
 
             //Prepare the final object for each element
@@ -100,14 +97,14 @@ export default class ProposalComparatorView extends React.Component {
                             />
                         </div>
                     }
-                    {debug(this.props.data)}
+                    {debug(this.props)}
                 </div>
             );
         }
         return (
             <div>
                 <LoadingAnimation />
-                {debug(this.props.data.data)}
+                {debug(this.props)}
             </div>);
     }
 }
